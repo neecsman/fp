00cf1604c732868a0e461609ae00c22a6acb17e0
Nikita <nikappleid@yandex.ru>
2023-04-11 11:47:55 +0300

    add pay

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -358,16 +358,12 @@ class OrderService {
   }
 
   async getStatus(id: any) {
-    try {
-      const order = await AppDataSource.getRepository(Orders).findOneBy({ id });
-
-      if (!order) {
-        throw ErrorService.BadRequest("Такого заказа не существует");
-      }
-      return order;
-    } catch (error) {
-      console.log(error);
+    const order = await AppDataSource.getRepository(Orders).findOneBy({ id });
+    if (!order) {
+      throw ErrorService.BadRequest("Такого заказа не существует");
     }
+
+    return order;
   }
 }
 
